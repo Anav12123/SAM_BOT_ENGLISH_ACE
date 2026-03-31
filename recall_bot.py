db1e99bde2b95b4ab09b3f441963ac939e1a3592
Anav12123 <sahil@anavcloudsoftwares.com>
2026-04-01 00:08:58 +0530

    Update recall_bot.py

--- recall_bot.py
+++ recall_bot.py
@@ -46,7 +46,7 @@ class RecallBot:
                         }
                     }
                 },
-                "audio_separate_raw": {},
+                "audio_mixed_raw": {},
                 "realtime_endpoints": [{
                     "type":   "websocket",
                     "url":    websocket_url,
@@ -57,7 +57,7 @@ class RecallBot:
                         "participant_events.speech_off",
                         "participant_events.join",
                         "participant_events.leave",
-                        "audio_separate_raw.data",
+                        "audio_mixed_raw.data",
                     ]
                 }]
             },
